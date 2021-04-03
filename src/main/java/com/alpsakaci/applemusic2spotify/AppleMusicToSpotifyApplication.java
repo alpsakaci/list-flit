6290c40d9d24205ec1ddb10421b9895eb61384a1
alpsakaci <alpsakaci@protonmail.com>
2021-04-03 21:27:34 +0300

    project init

---
+++ src/main/java/com/alpsakaci/applemusic2spotify/AppleMusicToSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.alpsakaci.applemusic2spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleMusicToSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleMusicToSpotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpsakaci/applemusic2spotify/AppleMusicToSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpsakaci.applemusic2spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleMusicToSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
