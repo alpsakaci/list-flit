c8b2c4136b0c1b2ed64484a2d5ffc7904dadbc0f
alpsakaci <alpsakaci@protonmail.com>
2021-04-05 14:29:53 +0300

    create track object

--- src/main/java/com/alpsakaci/applemusic2spotify/MusicLibraryController.java
+++ src/main/java/com/alpsakaci/applemusic2spotify/MusicLibraryController.java
@@ -10,29 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.w3c.dom.Node;
-
-import com.alpsakaci.applemusic2spotify.storage.StorageService;
 
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/")
 public class MusicLibraryController {
 
-	private final StorageService storageService;
-
 	@Autowired
 	private LibraryParseService libraryParseService;
 
-	@Autowired
-	public MusicLibraryController(StorageService storageService) {
-		this.storageService = storageService;
-	}
 
 	@PostMapping
 	@RequestMapping("/uploadLibrary")
 	void handleLibraryFileUpload(@RequestParam("libraryFile") MultipartFile file) {
-		List<Node> tracks = libraryParseService.getTracks(file);
+		List<Track> tracks = libraryParseService.getTracks(file);
 		System.out.println(tracks.size());
 	}
 
---
+++ src/main/java/com/alpsakaci/applemusic2spotify/Track.java
@@ -0,0 +1,56 @@
+package com.alpsakaci.applemusic2spotify;
+
+public class Track {
+
+	private int id;
+	private String name;
+	private String artist;
+	private String album;
+	
+	public Track() {}
+	
+	public Track(int id, String name, String artist, String album) {
+		this.id = id;
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	@Override
+	public String toString() {
+		return "Track [id=" + id + ", name=" + name + ", artist=" + artist + ", album=" + album + "]";
+	}
+	
+}
