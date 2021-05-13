7d22952a004d2fd081af29cf20887856cc6f7526
Alp Sakaci <alpsakaci@protonmail.com>
2021-05-13 16:57:30 +0300

    add spotify config component

---
+++ src/main/java/com/alpsakaci/applemusic2spotify/spotify/SpotifyApiConfig.java
@@ -0,0 +1,22 @@
+package com.alpsakaci.applemusic2spotify.spotify;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class SpotifyApiConfig {
+
+	private final String userPlaylistsUrl = "https://api.spotify.com/v1/me/playlists";
+
+	private final String searchTrackUrl = "https://api.spotify.com/v1/search?type=track&query=";
+
+	public String getUserPlaylistsUrl() {
+		return userPlaylistsUrl;
+	}
+
+	public String getSearchTrackUrl() {
+		return searchTrackUrl;
+	}
+
+}
