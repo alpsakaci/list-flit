9dcf3c9fb8cc08cb148bb2dbf37e244f98c78041
Alp Sakaci <alpsakaci@protonmail.com>
2021-05-24 23:47:42 +0300

    add spotify User and Image models

---
+++ src/main/java/com/alpsakaci/applemusic2spotify/spotify/model/User.java
@@ -0,0 +1,24 @@
+package com.alpsakaci.applemusic2spotify.spotify.model;
+
+public class User {
+
+	private String displayName;
+	private Image[] images;
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public Image[] getImages() {
+		return images;
+	}
+
+	public void setImages(Image[] images) {
+		this.images = images;
+	}
+
+}
