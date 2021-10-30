d985e0a691a91eee6e2ff87e06acdbd531e6b6ba
alpsakaci <alpsakaci@protonmail.com>
2021-10-30 11:56:46 +0300

    create docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM openjdk:16-jdk-alpine
 WORKDIR /usr/src/app
-COPY . .
-RUN ./mvnw package
-ENTRYPOINT ["./mvnw", "spring-boot:run"]
-
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT java -jar app.jar $JAVA_OPTS
---
+++ src/main/java/com/alpsakaci/listflit/config/OAuth2Config.java
@@ -0,0 +1,65 @@
+package com.alpsakaci.listflit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+
+@Configuration
+public class OAuth2Config extends WebSecurityConfigurerAdapter {
+
+	private SpotifyConfig spotifyConfig;
+
+	public OAuth2Config(SpotifyConfig spotifyConfig) {
+		this.spotifyConfig = spotifyConfig;
+	}
+	
+	private static final String[] PUBLIC_MATCHERS = {
+            "/",
+            "/css/**",
+            "/js/**",
+            "/img/**",
+    };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.cors().and().csrf().disable()
+			.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.oauth2Login();
+		// @formatter:on
+	}
+
+	@Bean
+	public ClientRegistrationRepository clientRegistrationRepository() {
+		return new InMemoryClientRegistrationRepository(this.spotifyClientRegistration());
+	}
+
+	private ClientRegistration spotifyClientRegistration() {
+		// @formatter:off
+		return ClientRegistration
+				.withRegistrationId("spotify")
+				.clientId(spotifyConfig.getClientId())
+				.clientSecret(spotifyConfig.getClientSecret())
+				.clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+				.scope("user-library-read", "playlist-modify-public", "playlist-modify-private")
+				.authorizationUri("https://accounts.spotify.com/authorize")
+				.tokenUri("https://accounts.spotify.com/api/token")
+				.userInfoUri("https://api.spotify.com/v1/me")
+				.userNameAttributeName("display_name")
+				.clientName("spotify")
+				.build();
+		// @formatter:on
+	}
+
+}
